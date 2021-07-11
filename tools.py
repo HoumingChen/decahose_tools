06d99eaa388a05e55c4d32917d3c861543311761
Houming Chen <houmingc@umich.edu>
2021-07-11 15:19:41 -0400

    updates

--- tools.py
+++ tools.py
@@ -1,9 +1,13 @@
-import glob
+from hdfs import Config
+
 
 class Tools():
     '''This class provides tools for getting decahose data'''
+
     def __init__(self):
         pass
 
     def get_file_names(self):
+        client = Config().get_client('dev')
+        files = client.list('the_dir_path')
         pass
