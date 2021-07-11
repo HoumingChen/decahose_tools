e717fd4d31f275665e75f985c5bdbe320b9c585f
Houming Chen <houmingc@umich.edu>
2021-07-11 15:33:36 -0400

    updates

--- open_spark.sh
+++ open_spark.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-export PYSPARK_PYTHON=/sw/dsi/aarch64/centos7/python/3.7.4/bin/python3
-export PYSPARK_DRIVER_PYTHON=/sw/dsi/aarch64/centos7/python/3.7.4/bin/python3
+virtualenv venv
+venv/bin/python install_requirements.py
+export PYSPARK_PYTHON=/venv/bin/python
+export PYSPARK_DRIVER_PYTHON=/venv/bin/python
 pyspark --master yarn --queue dhillon-group --num-executors 500 --executor-memory 5g --conf spark.ui.port=4069
