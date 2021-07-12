ba1ac08d6dc25f4f4fbadda47b8ee56f67a5f386
Houming Chen <houmingc@umich.edu>
2021-07-12 01:12:28 -0400

    updates

--- open_spark.sh
+++ open_spark.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export PYSPARK_PYTHON=./venv/bin/python
-export PYSPARK_DRIVER_PYTHON=./venv/bin/python
+export PYSPARK_PYTHON=$PWD/venv/bin/python
+export PYSPARK_DRIVER_PYTHON=$PWD/venv/bin/python
 pyspark --master yarn --queue dhillon-group --num-executors 500 --executor-memory 5g --conf spark.ui.port=4069
